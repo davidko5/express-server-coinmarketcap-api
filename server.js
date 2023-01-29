4ce46fe28455dfcb1ce74baaa017bd451beae70a
davidko <dajan526@gmail.com>
2023-01-29 18:05:56 +0100

    changed endpoint form / to /price-change and added /historicals for fetching historical data on some currencies

--- server.js
+++ server.js
@@ -79,6 +79,34 @@ app.get('/price-change', (req, res) => {
     });
     
 });
+app.get('/historicals', (req, res) => {
+  const axios = require('axios');
+
+  let response = null;
+  new Promise(async (resolve, reject) => {
+      try {
+          response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/historical?symbol=BTC,ETH,ADA&convert=USD', {
+          headers: {
+              'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+          },
+          });
+      } catch(ex) {
+          response = null;
+          // error
+          console.log(ex);
+          reject(ex);
+      }
+      if (response) {
+          // success
+          const json = response.data;
+          console.log(json);
+          // SENDS DATA BACK
+          res.send(json)
+          resolve(json);
+      }
+  });
+  
+});
 
 // listen on the port
 app.listen(port);
